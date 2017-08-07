7380a3e3f5a4befd8cc1cf307d1dff4e8c942ee7
Francisco Barrios Romo <pancho@MacBook-Pro-de-Francisco.local>
2017-08-07 18:22:13 -0400

    Version estable

--- header.php
+++ header.php
@@ -23,35 +23,5 @@
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php endif; ?>
 	<?php do_action( 'foundationpress_layout_start' ); ?>
-	<header class="site-header" role="banner">
-		<div class="row title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
-		  <div class="columns small-10">
-		  	 <img class="align-right"src="<?php bloginfo('template_directory'); ?>/assets/images/umbrella-logo.png"  alt="Space" style="width:233px ,height: 100px"></li>
-		  </div>
-		  <div class="columns small-2 align-right">
-		  	 <button class="menu-icon" type="button" data-toggle="example-menu"></button>
-		  </div>
-		</div>
-		<div class="top-bar" id="example-menu">
-		  <div class="top-bar-left hide-for-small-only">
-		    <ul class="dropdown menu" data-dropdown-menu>
-		      <li ><img src="<?php bloginfo('template_directory'); ?>/assets/images/umbrella-logo.png"  alt="Space" style="width:233px ,height: 100px"></li>
-		      <li>
-		    </ul>
-		  </div>
-		  <div class="top-bar-right">
-		  	<ul class="menu headermenu" id="example-menu">
-					  <li><a href="#">Familia Umbrella</a></li>
-					  <li><a href="#">Evidencia Cientifica</a></li>
-					  <li><a href="#">Blog</a></li>
-					  <li><a href="#">Contacto</a></li>
-					 <!--  <li><input type="search" placeholder="Search"></li>
-		      		  <li><button type="button" class="button">Search</button></li> -->
-			</ul>
-		  </div>
-		</div>
-	
-	</header>
 
-	<section class="container">
 	<?php do_action( 'foundationpress_after_header' );
--- index.php
+++ index.php
@@ -15,145 +15,78 @@
 
 get_header(); ?>
 
-<div class="row ">  
 
-<div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit >
-
-  <div class="orbit-wrapper">
-    <div class="orbit-controls">
-      <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-      <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
-    </div>
-    <ul class="orbit-container">
-      <?php rewind_posts();?>    <!-- reiniciar variables del loop php -->
-      <?php query_posts('post_per_pages=10') ?>
-      <?php query_posts('order=Desc&cat=8') ?> <!-- categoria orbit -->
-      <?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
-        <li class="is-active orbit-slide">
-          <figure class="orbit-figure">
-            <img class="orbit-image" src= <?php the_post_thumbnail('full') ?> >             
-            </img>
-    
-          </figure>
-        </li>
-      <?php endwhile; ?>
-
-      <?php else : ?>
-        <?php get_template_part( 'template-parts/content', 'none' ); ?>
-      <?php endif; // End have_posts() check. ?>
-
-    </ul>
-  </div>
-  <nav class="orbit-bullets">
-    <?php rewind_posts();?>    <!-- reiniciar variables del loop php -->
-    <?php query_posts('post_per_pages=10') ?>
-    <?php query_posts('order=Desc&cat=8') ?> <!-- categoria orbit -->
-    <?php 
-    $i = 0; 
-    if (have_posts()) : while ( have_posts() ) : the_post(); ?>
-     
-    <?php if (  $i ==  0 ) { ?>
-        <button class="is-active" data-slide=<?php $i ?> ><span class="show-for-sr"> <?php the_title() ?> </span></button>
-
-       <?php } else {  ?>
-         <button data-slide=<?php $i ?> ><span class="show-for-sr"> <?php the_title() ?> </span></button>
-        <?php } ?>
-    <?php
-    $i = $i +1;  
-    endwhile; ?>
-    <?php else : ?>
-          <?php get_template_part( 'template-parts/content', 'none' ); ?>
-    <?php endif; // End have_posts() check. ?>
-    <!-- <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
-    <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
-    <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button> -->
-  </nav>
-</div>
-</div>
-
-<!-- </span> que tu piel no acumule <br> más veranos Previene protege y repara la radiación solar en tu piel.</span>
- -->
-
-      <?php rewind_posts();?>    <!-- reiniciar variables del loop php -->
-      <?php query_posts('post_per_pages=10') ?>
-      <?php query_posts('order=Desc&cat=7') ?> <!-- categoria card2 -->
-      <?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
-<div class="row ">       
-        <div class="columns">
-          <div class="medium-6 columns card2">
-             <picture>
-              <?php the_post_thumbnail('full') ?>
-             </picture>
-          </div>
-          <div class="medium-6 columns  card2-section ">
-            <h5><?php the_title() ?></h5>
-            <p><?php the_excerpt(); ?></p>
-            <button class="hollow button" href="#">Ver más</button>
-          </div>
+<section id="javiera" class="callout large row align-middle">
+    <div class="row text-center">
+        <div class="small-12 columns show-for-small-only ">
+          <h2>Para Ipad iOS 10 y 9.</h2>
         </div>
-</div>
-      <?php endwhile; ?>
-
-      <?php else : ?>
-          <?php get_template_part( 'template-parts/content', 'none' ); ?>
-      <?php endif; // End have_posts() check. ?>
-
-
-
-
-<!-- <div class="row medium-up-2 ">
-  <div class="medium-6 columns">
-    <div class="card columns">
-      <img src="<?php bloginfo('template_directory'); ?>/assets/images/foto blog1.png" >
-      <div class="card-section">
-      	<h5>Titulo</h5>
-        <p>This card makes use of the card-divider element.</p>
-        <button class="hollow button" href="#">Ver más</button>
-      </div>
     </div>
-  </div>
-  <div class="medium-6 columns">
-    <div class="card columns">
-      <img src="<?php bloginfo('template_directory'); ?>/assets/images/foto blog 2.png" >
-      <div class="card-section">
-        <h5>Titulo</h5>
-        <p>This card makes use of the card-divider element.</p>
-        <button class="hollow button" href="#">Ver más</button>
-      </div>
-    </div>
-  </div>
-</div> -->
 
+    <div  class="row align-middle ">
+        <div class="small-6 columns align-center">
 
-<div class="row medium-up-2 ">
-      <?php rewind_posts();?>    <!-- reiniciar variables del loop php -->
-      <?php query_posts('post_per_pages=10') ?>
-      <?php query_posts('order=Desc&cat=6') ?> <!-- categoria card1 -->
-      <?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
-        <div class="medium-6 columns">
-          <div class="card columns">
-            <picture>
-              <?php the_post_thumbnail('full') ?>
-            </picture>
-              
-            <div class="card-section">
-                <h5><?php the_title() ?></h5>
-                <p><?php the_excerpt(); ?></p>
-                <!-- <?php the_category() ?> -->
-                <button class="hollow button" href="#">Ver más</button>
+            <?php rewind_posts();?>    <!-- reiniciar variables del loop php -->
+            <?php query_posts('post_per_pages=3 ') ?>
+            <?php query_posts('order=Desc&cat=3') ?> <!-- categoria card2 -->
+            <?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
+
+            <!-- <img  src= <?php the_post_thumbnail('full') ?> >  -->            
+          
+             <?php the_post_thumbnail('full') ?>
+             
+
+             <?php endwhile; ?>
+             <!-- <img data-interchange="[<?php get_post_gallery_images(11) ?>, 
+                                    <?php get_post_gallery_images(10) ?>, 
+                                      <?php get_post_gallery_images(12) ?>]" alt="device"  id="img">   -->
+            <?php else : ?>
+            <?php get_template_part( 'template-parts/content', 'none' ); ?>
+            <?php endif; // End have_posts() check. ?>
+        </div>
+        <div class="small-6 columns">
+          <?php rewind_posts();?>    <!-- reiniciar variables del loop php -->
+          <?php query_posts('post_per_pages=1 ') ?>
+          <?php query_posts('order=Desc&cat=5') ?> <!-- categoria card2 -->
+          <?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
+          <div class="row hide-for-small-only">
+              <h2><?php the_title()  ?></h2>
+          </div>
+          <div class="row">
+            <div class="callout text-justify">
+               <?php the_excerpt() ?>
             </div>
           </div>
-        </div>
-      <?php endwhile; ?>
-
-      <?php else : ?>
+          <?php endwhile; ?>
+          <?php else : ?>
           <?php get_template_part( 'template-parts/content', 'none' ); ?>
-      <?php endif; // End have_posts() check. ?>
-      
-</div>
+          <?php endif; // End have_posts() check. ?>
+          
+          <div class="tabs-content" data-tabs-content="lineup-tabs">
+                <div class="row text-center">
+                     <?php rewind_posts();?>    <!-- reiniciar variables del loop php -->
+                     <?php query_posts('post_per_pages=8 ') ?>
+                     <?php query_posts('order=Desc&cat=4') ?> <!-- categoria card2 -->
+                     <?php remove_filter ('the_content', 'wpautop'); ?>
+                     <?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
+                     <div class="small-6 columns appsRow">
+                        <figure  class="btn" onclick="location.href='itms-services://?action=download-manifest&url=<?php the_content() ?>'">
+                           <?php the_post_thumbnail('full') ?>
 
- 
+                        </figure>
+                     </div>
+                    <?php endwhile; ?>
+                    <?php else : ?>
+                    <?php get_template_part( 'template-parts/content', 'none' ); ?>
+                    <?php endif; // End have_posts() check. ?>
+                     
+                </div>
 
-</div>
+          </div>
+        </div>
+    </div>
+   
+</section>
+
   
 <?php get_footer();
